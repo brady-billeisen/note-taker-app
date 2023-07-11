312f414b4467d6fb5ba9f146ce21fb7a0b4261a6
brady <bradybill36@gmail.com>
2023-07-11 14:55:08 -0500

    got all the code done, just need to figure out how to deploy it onto heroku

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const path = require('path');
 const PORT = process.env.PORT ?? 3001;
 const app = express();
+const routes = require('./routes');
 
 app.use(express.static('public'));
-
-
-app.get('/notes', (req, res) => {
-    return res.sendFile(path.join(__dirname, 'public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    return res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`Server running @ http://localhost:${PORT}`);
